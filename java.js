eaf8ffa4dad7ce7cd9606a7d0912538edb94aee0
iqraashfaqiqra <112174330+iqraashfaqiqra@users.noreply.github.com>
2024-06-15 11:35:03 +0000

    c

---
+++ java.js
@@ -0,0 +1,15 @@
+//How js work
+/*
+sb sy phli line execute hony sy phly code compile hota hy 
+1->compile  
+        //compile 2 reason ki waja sy hota hy 
+            1->Early Error Checking 
+            2->Determing early variable scope checking (D.A.S.V)
+    Three compilation phase
+        1->Tokening/Lexing(hmra code choty choty part mayn divide hojata hy).
+        2->parsing(part koh use kr kay Abstract syntax tree bnaty(A.S.T)).
+        3->code Generation(A.S.T sy asa code generate krty jo agy Execute ho sky).
+2->Code Execuation Phase.
+    In J.s code execute inside execution context.
+    
+
