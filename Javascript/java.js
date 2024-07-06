66511bccfedfacea71a8dd50d27d3f04917943d3
iqraashfaqiqra <112174330+iqraashfaqiqra@users.noreply.github.com>
2024-07-06 10:21:07 +0000

    b

--- Javascript/java.js
+++ Javascript/java.js
@@ -47,3 +47,41 @@ innertext
          ids.setAttribute('id','Heading');
          console.log(ids.getAttribute('id'))
 */
+// ...................
+/*
+iterating Loop
+1-loop
+2- for-of
+3-forEach(use with nodelist only)(can't used with HTML Collection)
+    if we want to apply foreach on HTML COllection
+                    let ary=document.getElementsByClassName('nav-item')
+                    console.log(ary);
+                    ary=Array.from(ary)
+                    ary.forEach(nav=>console.log(nav)
+            )
+*/
+// .......................
+/* innerHTML
+        const a=document.querySelector('h2');
+        a.innerHTML="<h1>Iqra Ashfaq</h2>"
+*/
+// ............................
+/*
+1-ChildNode----> will show new line also [head,text,body], head=> [text,title,text], body=>[text,h1,text,p,text]
+2-children-----> will show only element [head,body],head=>[title],body=>[h1,p]
+3-parentElement,parentNode---->same parent 
+4-NextSibling---->will show text node also
+5-nextElementSibling------>not show text node.
+*/
+            // const rootnode=document.getRootNode();
+            // console.log(rootnode.children)   //output:HTML Collection
+            // console.log(rootnode.childNodes) //output:Nodelist
+            // const htmlElementNode=rootnode.childNodes[1];
+            // console.log(htmlElementNode.childNodes)  //[head,text,body]
+            // // const headElementNode=htmlElementNode.childNodes[0] //0=head,1=text,2=body
+            // // console.log(headElementNode);
+            // const bodyElementNode=htmlElementNode.childNodes[2] //0=head,1=text,2=body
+            // console.log(bodyElementNode.childNodes);
+            // const navs=document.querySelector('.nav-items').childNodes[1];
+            // console.log(navs.nextElementSibling)
+// .............................. 
